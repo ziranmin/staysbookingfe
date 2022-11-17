9390588835910e11b33afc9d59c3f43d385f294a
ziranmin <minziran@bu.edu>
2022-11-17 17:27:38 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Layout, Dropdown, Menu, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import React from "react";
+
+import LoginPage from "./components/LoginPage";
+import HostHomePage from "./components/HostHomePage";
+import GuestHomePage from "./components/GuestHomePage";
+
+const { Header, Content } = Layout;
+ 
+class App extends React.Component {
+  state = {
+    authed: false,
+    asHost: false,
+  };
+ 
+  componentDidMount() {
+    const authToken = localStorage.getItem("authToken");
+    const asHost = localStorage.getItem("asHost") === "true";
+    this.setState({
+      authed: authToken !== null,
+      asHost,
+    });
+  }
+ 
+  handleLoginSuccess = (token, asHost) => {
+    localStorage.setItem("authToken", token);
+    localStorage.setItem("asHost", asHost);
+    this.setState({
+      authed: true,
+      asHost,
+    });
+  };
+ 
+  handleLogOut = () => {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("asHost");
+    this.setState({
+      authed: false,
+    });
+  };
+ 
+  renderContent = () => {
+    if (!this.state.authed) {
+      //return <div>login page</div>;
+      return <LoginPage handleLoginSuccess={this.handleLoginSuccess} />;
+    }
+ 
+    if (this.state.asHost) {
+      //return <div>host home page</div>;
+      return <HostHomePage />;
+    }
+ 
+    //return <div>guest home page</div>;
+    return <GuestHomePage />;
+  };
+ 
+  userMenu = (
+    <Menu>
+      <Menu.Item key="logout" onClick={this.handleLogOut}>
+        Log Out
+      </Menu.Item>
+    </Menu>
   );
-}
-
+ 
+  render() {
+    return (
+      <Layout style={{ height: "100vh" }}>
+        <Header style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
+            Z-Booking
+          </div>
+          {this.state.authed && (
+            <div>
+              <Dropdown trigger="click" overlay={this.userMenu}>
+                <Button icon={<UserOutlined />} shape="circle" />
+              </Dropdown>
+            </div>
+          )}
+        </Header>
+        <Content
+          style={{ height: "calc(100% - 64px)", margin: 20, overflow: "auto" }}
+        >
+          {this.renderContent()}
+        </Content>
+      </Layout>
+    );
+  }
+} 
+ 
 export default App;
